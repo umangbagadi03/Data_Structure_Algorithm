6f7e1f3c89f74386fdedafac1f768d0aae03b73b
Umang Bagadi <78692913+umangbagadi03@users.noreply.github.com>
2022-10-19 00:48:20 +0530

    walkingExampleHome.cpp

---
+++ Recursion/walkingExampleHome.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+
+// destination 
+void reachHome(int src, int destination) {
+
+    // base case
+    if(curr_loc == destination) {
+
+        cout << "poch gaya ghar" << endl;
+        return ;
+    }
+    // processsing ekk step aagey badh jao
+    curr_loc++;
+
+    // recursive call
+    reachHome(curr_loc, destination);
+}
+
+int main( ) {
+    int destination = 40;
+    int curr_loc = 1;
+
+    cout << endl;
+
+    reachHome(curr_loc, destination);
+
+    return 0;
+}
