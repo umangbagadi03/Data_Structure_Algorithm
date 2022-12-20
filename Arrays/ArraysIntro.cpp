68ea73666dbbbc3f8d38290998c6f170b6bdf6ca
Umang Bagadi <78692913+umangbagadi03@users.noreply.github.com>
2022-12-20 19:39:16 +0530

    Added Arrays

---
+++ Arrays/ArraysIntro.cpp
@@ -0,0 +1,16 @@
+// arrays in c++
+
+#include<iostream>
+using namespace std;
+
+
+
+int main() {
+
+    // declare 
+    int number[15];  // 15 size hai uske array ki
+
+    // accessing an array
+    cout<<"value at 14 index"<< endl;
+
+}
---
+++ Arrays/MinMaxArrays.cpp
@@ -0,0 +1,49 @@
+// to find min max in arrays 
+
+#include<iostream>
+using namespace std;
+
+int getMin(int num[], int n) {
+    int mini = INT_MAX;
+
+    for(int i = 0, i < n, i++){
+        if(num[i]<min){
+            min = num[i]; 
+        }
+    }
+
+    // returning min value
+    return mini;
+}
+
+// now for max 
+int getMax(int num[], int n) {
+    int maxi = INT_MIN;
+    for(int i = 0; i<n; i++) {
+        if(num[i] > max) {
+            max = num[i]
+        }
+    }
+
+    // returning max values
+    return maxi;
+}
+
+int main() {
+    int size;
+    cin >> size;
+
+    int num[100];
+
+    //taking input in array
+    for(int i = 0; i<size; i++){
+        cin >> num[i];
+
+    }
+
+    cout << " Maximum value is " << getMax(num, size) << endl;
+    cout << " Minimum value is " << getMin(num, size) << endl;
+
+
+    return 0;
+}
