092720c42de499fb3ac3e617cdc0b3a46c297ac6
Umang Bagadi <78692913+umangbagadi03@users.noreply.github.com>
2022-12-22 18:20:12 +0530

    added peakInMountainArray.cpp

---
+++ Arrays/peakInMountainArray.cpp
@@ -0,0 +1,27 @@
+// leetcode
+// our array is in the form of mountain we have to find the peak elemnt in it 
+
+#include<v>
+using namespace std;
+
+class solution {
+public:
+    int find_pivot(vector<int> v) {
+	int s = 0, e = v.size() - 1;
+	int mid=(s + e) / 2;
+	while (s < e)
+	{
+		if(v[mid] < v[mid+1])
+     			s=mid+1;
+    		else
+      			e = mid;
+    
+		mid = (s + e) / 2;
+	}
+	return s;
+    }
+    int peakIndexInMountainArray(vector<int>& arr) {
+        return find_pivot(arr);
+    }
+
+};
