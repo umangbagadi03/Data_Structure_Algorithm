951c2f03551bf93a2a92c5870b08efbc878f0e70
Umang Bagadi <78692913+umangbagadi03@users.noreply.github.com>
2022-12-26 22:47:39 +0530

    added insertionSort

---
+++ InsertionSort/insertionSort.cpp
@@ -0,0 +1,26 @@
+// insertion sort in cpp
+
+#include<vector>
+using namespace std;
+
+void insertionsort(int n, vector<int> &arr) {
+    for(int i = 1; i<n; i++) {
+        int temp = arr[i];
+        int j = i-1;
+        for(; j>=0; j--) {
+             if(arr[j] > temp) {
+                //shift
+                arr[j+1] = arr[j];
+            }
+            else { // ruk jao
+                break;
+            }
+            
+        }
+        //copy temp value
+        arr[j+1] = temp;  
+
+
+        
+    }
+}
