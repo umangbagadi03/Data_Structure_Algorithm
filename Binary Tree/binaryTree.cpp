21c7643de2a4c447b74d10bcacd15fe59baced0f
Umang Bagadi <78692913+umangbagadi03@users.noreply.github.com>
2023-01-14 18:49:20 +0530

    Add files via upload

---
+++ Binary Tree/binaryTree.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+class node {
+    public :
+        int data;
+        node* left;
+        node* right;
+
+    node(int d) {
+        this -> data = d;
+        this -> left = NULL;
+        this -> right = NULL;
+    }
+};
+
+node* buildTree(node* root) {
+
+    cout << "Enter the data: " << endl;
+    int data;
+    cin >> data;
+    root = new node(data);
+
+    if(data == -1) {
+        return NULL;
+    }
+
+    cout << "Enter data for inserting in left of " << data << endl;
+    root -> left = buildTree(root -> left);
+    cout << "Enter data for inserting in right of " << data << endl;
+    root -> right = buildTree(root -> right);
+
+}
+
+void levelOrderTraversal(node* root) {
+    queue<node*> q;
+    q.push(root);
+    q.push(NULL);
+
+    while(!q.empty()) {
+        node* temp = q.front();
+        q.pop();
+
+        if(temp == NULL) {
+            // purana level complete traverse ho chuka hai
+            cout << end;
+            if(!q.empty()) {
+                // queue still has some child node
+                q.push(NULL);
+            }
+        }
+        else {
+            cout << temp -> data << " ";
+            if(temp -> left) {
+                q.push(temp -> left);
+            }
+
+            if(temp -> right){
+                q.push(temp -> right);
+            }
+        }
+    }
+}
+
+int main() {
+    node* root = NULL;
+
+    buildFromLevelOrder(root);
+    levelOrderTraversal(root);
+
+    return 0;
+}
