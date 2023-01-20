d98ecfa08854e4b6ef2ccb5a6e70b074bd41e74f
Umang Bagadi <78692913+umangbagadi03@users.noreply.github.com>
2023-01-20 20:05:38 +0530

    Update LargestBST.cpp

--- Binary Search Tree/LargestBST.cpp
+++ Binary Search Tree/LargestBST.cpp
@@ -1,3 +1,17 @@
+/*
+This code defines a class 'TreeNode' which represents a node in a binary tree, with an integer 'data', and two pointers to its left and right children. It also defines a class 'info' that holds some properties of a binary tree.
+
+The main function 'largestBST' takes a pointer to the root of a binary tree and finds the largest sub-tree in the binary tree that is a binary search tree. It does this by calling the helper function 'solve' which takes a pointer to a node in the tree, and an integer 'ans' which will store the size of the largest sub-tree.
+
+The function 'solve' starts by handling the base case when the input node is NULL. It then recursively calls itself on the left and right children of the input node, obtaining 'info' objects for each subtree.
+
+Next, it sets the properties of the current node such as the size, maximum and minimum values. Then it checks if the current subtree is a binary search tree by checking if the left and right subtrees are binary search trees and the current node's value is within the range of its left and right subtrees.
+
+If the current subtree is a binary search tree, it updates the 'ans' variable with the size of the current subtree if it is larger than the previous largest subtree. Finally, it returns an 'info' object for the current subtree.
+
+The 'largestBST' function returns the value stored in the 'ans' variable, which is the size of the largest binary search subtree.
+*/
+
 class TreeNode {
     public:
     T data;
