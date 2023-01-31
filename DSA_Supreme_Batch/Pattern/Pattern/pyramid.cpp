e66bf329418412c6d5af3a106469f1de1d9e836d
Umang Bagadi <78692913+umangbagadi03@users.noreply.github.com>
2023-01-31 16:08:07 +0530

    Update pyramid.cpp

--- DSA_Supreme_Batch/Pattern/Pattern/pyramid.cpp
+++ DSA_Supreme_Batch/Pattern/Pattern/pyramid.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 
 int main(){
-    int rows;
+    int n;
     cin >> n;
-    for(row = 0; row < n; row = row + 1){
-        for(col = 0; col < row + 1; col = col + 1)  //
+    for(int row = 0; row < n; row = row + 1){
+        for(int col = 0; col < row + 1; col = col + 1)  //
     }
     cout << "* ";
 }
