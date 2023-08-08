21d977a7d3853faa1ea7e927010cf3ddf4f06af3
Umang Bagadi <78692913+umangbagadi03@users.noreply.github.com>
2023-08-08 13:36:08 -0700

    Update selectionSort.cpp

--- SelectionSort/selectionSort.cpp
+++ SelectionSort/selectionSort.cpp
@@ -2,19 +2,28 @@
 using namespace std;
 void selectionSort(vector<int>& arr,int n);
 {
-    for(int i,i<n-1,i++) {   // jo pahila hai usko apan n i consider kar liya aur usko sab se chota mana hai abhi k liye jo i+1 elemnt and uske agey k ha unko hi swap krnege apn isko aisa hi rakhenege ye smj k ki ye sabse chota hai
+    for(int i, i<n-1, i++) {   // jo pahila hai usko apan n i consider kar liya aur usko sab se chota mana hai abhi k liye jo i+1 elemnt and uske agey k ha unko hi swap krnege apn isko aisa hi rakhenege ye smj k ki ye sabse chota hai
         int minIndex = i;  //so min index i ko pakda apn n
 
-
-        for(int j,j<n,j++){   // apn n pahle wale ko consider kiya k vo sabse chota ha aur sorted lekin jo next hai usko apan n j liya hai aur j++ krke aagey ja rhe
+        for(int j = i+1;, j<n, j++){   // apn n pahle wale ko consider kiya k vo sabse chota ha aur sorted lekin jo next hai usko apan n j liya hai aur j++ krke aagey ja rhe
             if(arr[j]<arr[minIndex]) // lekin agr ye 2 index k agey k hi chote niklna toh 
             minIndex = j;            // toh fhir usko hi minindex consider krna
 
         }
         swap(arr[minIndex], arr[i]);
-
-    
-
     }
-
+}
+int main() {
+    int arr[] = {80, 70, 60, 50, 40, 30, 20, 10};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    
+    SelectionSort(arr, n);
+    
+    cout << "Sorted Array is : ";
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    
+    return 0;
 }
