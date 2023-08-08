f34a033d3af8b5a7c1e59272337620233da5e42e
Umang Bagadi <78692913+umangbagadi03@users.noreply.github.com>
2023-08-08 16:07:06 -0700

    Update insertionSort.cpp

--- InsertionSort/insertionSort.cpp
+++ InsertionSort/insertionSort.cpp
@@ -1,26 +1,34 @@
-// insertion sort in cpp
-
+#include<iostream>
 #include<vector>
+
 using namespace std;
 
-void insertionsort(int n, vector<int> &arr) {
-    for(int i = 1; i<n; i++) {
+void insertionSort(int n, vector<int> &arr){
+    int i, j;
+    for(int i = 1; i<n; i++){
         int temp = arr[i];
         int j = i-1;
-        for(; j>=0; j--) {
-             if(arr[j] > temp) {
-                //shift
+        while(j>=0){
+            if(arr[j] > temp){
                 arr[j+1] = arr[j];
+                j--;
             }
-            else { // ruk jao
+            else {
                 break;
             }
-            
         }
-        //copy temp value
-        arr[j+1] = temp;  
-
-
-        
+        arr[j+1] = temp;
     }
+}
+
+int main(){
+    int arr[] = {10,20,40,30,50,80,60};
+    int n = sizeof(arr) / sizeof(arr[0]); // Calculate the size of the array
+    
+    insertionSort(n, arr);
+    
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 }
