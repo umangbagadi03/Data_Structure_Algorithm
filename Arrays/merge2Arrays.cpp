0a97bcd909cdc58c32fa002b1c9abd1e9aa6363e
Umang Bagadi <78692913+umangbagadi03@users.noreply.github.com>
2023-08-12 15:21:45 -0700

    Update merge2Arrays.cpp

--- Arrays/merge2Arrays.cpp
+++ Arrays/merge2Arrays.cpp
@@ -1,62 +1,59 @@
 // merge 2 sorted arrays
 
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <limits.h>
 using namespace std;
 
-void merge(int arr1[], int n, inr arr2[], int m, int arr3[]) {
-    int i = 0, j = 0;
-    int k = 0;
-    while (i < n, && j < m) {
-        if(arr1[i] < arr2[j]) {
-            arr3[k] = arr1[i]
-            j++;
+// Function to merge two sorted arrays arr1 and arr2 into arr3
+void Merge2Arrays(int arr1[], int n, int arr2[], int m, int arr3[]) {
+    int i = 0, j = 0, k = 0;
+
+    // Compare elements from arr1 and arr2 and merge into arr3
+    while (i < n && j < m) {
+        if (arr1[i] < arr2[j]) {
+            arr3[k] = arr1[i];
+            i++;
             k++;
-        }
-        else {
+        } else {
             arr3[k] = arr2[j];
             j++;
             k++;
         }
     }
 
-    // code to copy first array elemnt
-    while(i<n) {
-        arr3[k] = arr[i];
+    // If there are remaining elements in arr1, copy them to arr3
+    while (i < n) {
+        arr3[k] = arr1[i];
         i++;
         k++;
     }
 
-    // code to copy second array elemnt
-    while(j<m) {
-        arr3[k] = arr[j];
+    // If there are remaining elements in arr2, copy them to arr3
+    while (j < m) {
+        arr3[k] = arr2[j];
         j++;
         k++;
     }
-
-    // function to print
-    void print(int ans[], int n) {
-        for(int i=0; i<n; i++) {
-        cout<< ans[i] <<" ";
-    }
-    cout << endl;
-
-    }
 }
 
-
+// Function to print the elements of an array
+void print(int ans[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << ans[i] << " ";
+    }
+    cout << endl;
+}
 
 int main() {
-
-    // initialize arrays to be merged
-    int arr1[5] = {1,2,4,6,7};
-    int arr2[3] = {3,5,8}
-
+    int arr1[5] = {1, 2, 4, 6, 7};
+    int arr2[3] = {3, 5, 8};
     int arr3[8] = {0};
 
-    merge(arr1, 5 , arr2, 3, arr3);  // calling merge fun
+    // Merge arr1 and arr2 into arr3
+    Merge2Arrays(arr1, 5, arr2, 3, arr3);
 
-    print(arr3, 8);   // calling print func\
+    // Print the merged array arr3
+    print(arr3, 8);
 
     return 0;
 }
