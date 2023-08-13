a322de501a9438998486cea9a786ee2e8b153033
Umang Bagadi <78692913+umangbagadi03@users.noreply.github.com>
2023-08-13 07:52:28 -0700

    Update reverseString.cpp

--- Recursion/reverseString.cpp
+++ Recursion/reverseString.cpp
@@ -1,28 +1,27 @@
 // to reverse string using 
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 void reverse(string& str, int i, int j) {
-
     // base case
-    if(i>j){
-        return ;
-
+    if (i >= j) {
+        return;
     }
+
     swap(str[i], str[j]);
-    j++;
-    i--;
+    i++;
+    j--;
 
-    // recursive call 
-    reverse(str,i,j);
+    // recursive call
+    reverse(str, i, j);
 }
-
 
 int main() {
     string name = "umang";
-    reverse(name, 0, name.length()-1);
+    reverse(name, 0, name.length() - 1);
     cout << name << endl;
 
     return 0;
 }
+
